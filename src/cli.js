df0d3f84482c26b5ed2cdecfe912a61e4e09e84a
Viktor <vik333444@yandex.ru>
2021-08-02 20:20:05 +0400

    fix file

--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,6 @@ const hello = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
+  return name;
 };
 export default hello;
