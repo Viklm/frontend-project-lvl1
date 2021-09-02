1a22249140727c67eda6ee06cbae80dd38f845b3
Viktor <vik333444@yandex.ru>
2021-09-02 18:37:39 +0400

    fixed error

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -4,7 +4,7 @@ import randomOfNum from '../randomOfNum.js';
 const description = 'Find the greatest common divisor of given numbers.';
 const getGreatestCommonDivisor = (num1, num2) => {
   let result = 0;
-  for (let i = 1; i < num1 / 2 || i < num2 / 2; i += 1) {
+  for (let i = 1; i <= num1 || i <= num2; i += 1) {
     if (num1 % i === 0 && num2 % i === 0) {
       result = i;
     }
